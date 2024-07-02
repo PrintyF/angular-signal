79ecfc6b29c3cd81484ec71496d5fcd2e6d0dec8
artalon <felix.pinty@gmail.com>
2024-07-02 16:35:14 +0200

    mise en place du code en observabilité

--- angular-signal/src/app/app.component.ts
+++ angular-signal/src/app/app.component.ts
@@ -3,12 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ChildComponent } from './child/child.component';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule, ChildComponent],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, ChildComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  $value: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  changeValue(): void {
+    this.$value.next(Math.random()*10);
+  }
 }
--- angular-signal/src/app/child/child.component.ts
+++ angular-signal/src/app/child/child.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'child',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss'
 })
 export class ChildComponent {
-  @Input('name') name: string = '';
+  @Input('name') name: string = ''; 
+  @Input('value') value: BehaviorSubject<number> | null = null;
+
+  changeValue(): void {
+    this.value?.next(Math.random()*10);
+  }
 }
